1f6e7a88cfb272c3e6d0eae2b72d68dd5c231eda
rob <robertchen234@gmail.com>
2019-04-07 14:48:35 -0400

    navbar links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Route, Switch, withRouter } from "react-router-dom";
+import NavBar from "./components/NavBar";
 import Work from "./containers/Work";
 import Resume from "./containers/Resume";
 import About from "./containers/About";
@@ -10,8 +11,8 @@ class App extends Component {
   render() {
     return (
       <div>
+        <NavBar />
         <Switch>
-          <Route path="/work" component={Work} />
           <Route path="/resume" component={Resume} />
           <Route path="/about" component={About} />
           <Route path="/contact" component={Contact} />
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,74 @@
 import React, { Component } from "react";
-// import { withRouter, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 class NavBar extends Component {
+  state = { currentLink: "work" };
+
+  clickHandler = path => {
+    this.setState({ currentLink: path });
+  };
+
   render() {
     return (
       <div className="navbar">
         <div className="navbar-left">
-            <p>Work</p>
-            <p>Resume</p>
+          <p>
+            <Link
+              to="/"
+              className={
+                this.state.currentLink === "work" ? "currentLink" : null
+              }
+              onClick={() => this.clickHandler("work")}
+            >
+              Work
+            </Link>
+          </p>
+          <p>
+            <Link
+              to="/resume"
+              className={
+                this.state.currentLink === "resume" ? "currentLink" : null
+              }
+              onClick={() => this.clickHandler("resume")}
+            >
+              Resume
+            </Link>
+          </p>
         </div>
         <div className="navbar-mid">
-          <h3>IRIS Z LEE</h3>
+          <h3>
+            <Link
+              to="/"
+              className="currentLink"
+              onClick={() => this.clickHandler("work")}
+            >
+              IRIS Z LEE
+            </Link>
+          </h3>
         </div>
         <div className="navbar-right">
-            <p>About</p>
-            <p>Contact</p>
+          <p>
+            <Link
+              to="/about"
+              className={
+                this.state.currentLink === "about" ? "currentLink" : null
+              }
+              onClick={() => this.clickHandler("about")}
+            >
+              About
+            </Link>
+          </p>
+          <p>
+            <Link
+              to="/contact"
+              className={
+                this.state.currentLink === "contact" ? "currentLink" : null
+              }
+              onClick={() => this.clickHandler("contact")}
+            >
+              Contact
+            </Link>
+          </p>
         </div>
       </div>
     );
