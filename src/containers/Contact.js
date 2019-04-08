4210d8201642e1f7a1ebdc4779b9ed04c53b0570
rob <robertchen234@gmail.com>
2019-04-08 13:55:54 -0400

    contact form flex

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 
 class Contact extends Component {
-  state = { input: "", email: "", message: "" };
+  state = { subject: "", email: "", message: "" };
 
-  changeHandler = input => {
-    this.setState({ input });
+  changeHandler = (e, input) => {
+    this.setState({ [e.target.name]: input });
   };
 
   submitHandler = () => {};
@@ -18,46 +18,51 @@ class Contact extends Component {
         <div className="contact">
           <p>
             Find me on social media, use the form below, or hit me up via email
-            at iris.lee129@gmail.com.
+            at <a href="mailto:iris.lee129@gmail.com">iris.lee129@gmail.com</a>.
           </p>
 
           <form onSubmit={this.submitHandler}>
-          <div className="contact-email">
-            <label htmlFor="email">Email Address: *</label>
+            <div className="contact-email">
+              <label htmlFor="email">Email Address: *</label>
+              <br />
+              <input
+                id="email"
+                className="form-control"
+                name="email"
+                type="text"
+                value={this.state.email}
+                onChange={this.changeHandler}
+              />
+            </div>
             <br />
-            <input
-              id="email"
-              className="form-control"
-              name="email"
-              type="text"
-              value={this.state.email}
-              onChange={this.changeHandler}
-            /></div>
+            <div className="contact-subject">
+              <label htmlFor="subject">Subject: *</label>
+              <br />
+              <input
+                id="subject"
+                className="form-control"
+                name="subject"
+                type="text"
+                value={this.state.subject}
+                onChange={this.changeHandler}
+              />
+            </div>
             <br />
-            <div className="contact-subject"><label htmlFor="subject">Subject: *</label>
-            <br />
-            <input
-              id="subject"
-              className="form-control"
-              name="subject"
-              type="text"
-              value={this.state.subject}
-              onChange={this.changeHandler}
-            /></div>
-            <br />
-            <div className="contact-message"><label htmlFor="message">Message: *</label>
-            <br />
-            <textarea
-              id="message"
-              className="form-control"
-              name="message"
-              type="text"
-              // cols="1200"
-              // rows="1"
-              // maxLength="600"
-              value={this.state.message}
-              onChange={this.changeHandler}
-            /></div>
+            <div className="contact-message">
+              <label htmlFor="message">Message: *</label>
+              <br />
+              <textarea
+                id="message"
+                className="form-control"
+                name="message"
+                type="text"
+                rows="6"
+                // cols="1200"
+                // maxLength="600"
+                value={this.state.message}
+                onChange={this.changeHandler}
+              />
+            </div>
             <br />
 
             <button className="pointer" type="submit">
